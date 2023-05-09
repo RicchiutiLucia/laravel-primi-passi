152035df17d70bb9df691a5de1a2df7499996a2e
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-09 15:21:57 +0200

    bonus completato

--- resources/views/contacts.php
+++ resources/views/contacts.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Laravel primi passi</title>
+    <title>Contacts</title>
 </head>
+
 <body>
-    <header>
+<header>
         <nav class="navbar navbar-expand-lg bg-dark">
             <div class="container-fluid">
                 <a class="navbar-brand text-white" href="#">Navbar</a>
@@ -20,26 +22,35 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="{{ route('home') }}">Home</a>
+                            <a class="nav-link active text-white" href="/">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white" href="{{ route('contacts') }}">Contatti</a>
                         </li>
+                        
+                       
+                       
                     </ul>
                     
                 </div>
             </div>
         </nav>
     </header>
-
-
     <main class="p-3 text-center">
-        <h1>Benvenuto User!!</h1>
+        <h1>Pagina contatti</h1>
 
-        <h3>Nome: {{$name}}</h3>
-        <h3>Cognome: {{$surname}}</h3>
+        <h3>Email: lucia@email.it</h3>
+        <h3>Phone: 2514368</h3>
+        <h3>City: Bari</h3>
+       
     </main>
 
+    
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+   
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home',['name' => 'Lucia'], ['surname' => 'Ricchiuti']);
-});
+})->name('home');
+
+
+Route::get('/contacts', function () {
+   
+    return view('contacts');
+})->name('contacts');
+
+
